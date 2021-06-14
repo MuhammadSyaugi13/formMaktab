7b3f0bf297d62105a980e18f949d37f25ab7660c
MuhammadSyaugi13 <muhammadsyaugi13@gmail.com>
2021-06-14 20:22:02 +0700

    merapihkan form dalam modal dan merapikan baris2 html

--- formScript.js
+++ formScript.js
@@ -3,3 +3,19 @@ function ambilNama() {
     alert(data);
 
 }
+
+//cek ukuran browser
+const width = window.innerWidth;
+
+if (width < 400) {
+
+    let form1 = document.querySelector('.formulir1');
+    form1.innerHTML = '<br>';
+
+    let form2 = document.querySelector('.formulir2');
+    form1.innerHTML = '<br>';
+
+
+
+}
+
